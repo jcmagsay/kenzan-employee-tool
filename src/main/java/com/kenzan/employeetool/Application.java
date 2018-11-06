25993bd153f7a0e5ed0c98ba5750ac9cba1958df
Jill Magsaysay <jcmagsay@gmail.com>
2018-11-06 00:59:15 -0700

    Update Application.java
    Remove unnecessary code

--- src/main/java/com/kenzan/employeetool/Application.java
+++ src/main/java/com/kenzan/employeetool/Application.java
@@ -12,33 +12,4 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-    // @Override
-    // public void run(String... args) throws Exception {
-    //
-    //     repository.deleteAll();
-    //
-    //     // save a couple of employees
-    //     repository.save(new Employee("Alice", "Smith"));
-    //     repository.save(new Employee("Bob", "Smith"));
-    //
-    //     // fetch all employees
-    //     System.out.println("Employees found with findAll():");
-    //     System.out.println("-------------------------------");
-    //     for (Employee employee : repository.findAll()) {
-    //       System.out.println(employee);
-    //     }
-    //     System.out.println();
-    //
-    //     // fetch an individual employee
-    //     System.out.println("Employee found with findByFirstName('Alice'):");
-    //     System.out.println("--------------------------------");
-    //     System.out.println(repository.findByFirstName("Alice"));
-    //
-    //     System.out.println("Employees found with findByLastName('Smith'):");
-    //     System.out.println("--------------------------------");
-    //     for (Employee employee : repository.findByLastName("Smith")) {
-    //       System.out.println(employee);
-    //     }
-    // }
 }
